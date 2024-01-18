e0cb3b62e0b981e9278c514d39a5a481bd17b261
Pascal Thomet <pthomet@gmail.com>
2024-01-18 23:47:10 +0100

    copy dock structs

--- bindings/pybind_imgui.cpp
+++ bindings/pybind_imgui.cpp
@@ -7,16 +7,64 @@
 
 
 #include "imgui.h"
+#include "imgui_internal.h"
 
 namespace py = pybind11;
 
-// ImGuiContext is published as an opaque structure.
-// We only reproduce its constructor API here.
-struct ImGuiContext
+//
+// We have to import private declarations from imgui.cpp, in order to be able to
+// bind ImGuiContext. Below are verbatim copies of 3 structs from imgui.cpp:
+// ImGuiDockRequestType, ImGuiDockRequest, ImGuiDockNodeSettings
+// (minus the constructors implementations)
+
+enum ImGuiDockRequestType
 {
-  // Opaque
-  ImGuiContext(ImFontAtlas*);
+    ImGuiDockRequestType_None = 0,
+    ImGuiDockRequestType_Dock,
+    ImGuiDockRequestType_Undock,
+    ImGuiDockRequestType_Split                  // Split is the same as Dock but without a DockPayload
 };
+
+struct ImGuiDockRequest
+{
+    ImGuiDockRequestType    Type;
+    ImGuiWindow*            DockTargetWindow;   // Destination/Target Window to dock into (may be a loose window or a DockNode, might be NULL in which case DockTargetNode cannot be NULL)
+    ImGuiDockNode*          DockTargetNode;     // Destination/Target Node to dock into
+    ImGuiWindow*            DockPayload;        // Source/Payload window to dock (may be a loose window or a DockNode), [Optional]
+    ImGuiDir                DockSplitDir;
+    float                   DockSplitRatio;
+    bool                    DockSplitOuter;
+    ImGuiWindow*            UndockTargetWindow;
+    ImGuiDockNode*          UndockTargetNode;
+
+    ImGuiDockRequest();
+//    {
+//        Type = ImGuiDockRequestType_None;
+//        DockTargetWindow = DockPayload = UndockTargetWindow = NULL;
+//        DockTargetNode = UndockTargetNode = NULL;
+//        DockSplitDir = ImGuiDir_None;
+//        DockSplitRatio = 0.5f;
+//        DockSplitOuter = false;
+//    }
+};
+
+struct ImGuiDockNodeSettings
+{
+    ImGuiID             ID;
+    ImGuiID             ParentNodeId;
+    ImGuiID             ParentWindowId;
+    ImGuiID             SelectedTabId;
+    signed char         SplitAxis;
+    char                Depth;
+    ImGuiDockNodeFlags  Flags;                  // NB: We save individual flags one by one in ascii format (ImGuiDockNodeFlags_SavedFlagsMask_)
+    ImVec2ih            Pos;
+    ImVec2ih            Size;
+    ImVec2ih            SizeRef;
+    ImGuiDockNodeSettings();
+    //ImGuiDockNodeSettings() { memset(this, 0, sizeof(*this)); SplitAxis = ImGuiAxis_None; }
+};
+
+
 
 
 void py_init_module_imgui(py::module& m)
