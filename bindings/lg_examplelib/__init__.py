ba42fcf0c5e1ff10da83a1ba0bd3070c182c659a
Pascal Thomet <pthomet@gmail.com>
2024-01-18 21:17:42 +0100

    dbg_u: work on imgui

--- bindings/lg_examplelib/__init__.py
+++ bindings/lg_examplelib/__init__.py
@@ -1,2 +1,1 @@
 from lg_examplelib._lg_examplelib import *  # noqa: F403
-from lg_examplelib._lg_examplelib import __version__  # noqa: F401
--- external/examplelibcpp/examplelibcpp.cpp
+++
@@ -1,9 +0,0 @@
-#include "examplelibcpp/examplelibcpp.h"
-
-namespace examplelib
-{
-    int add(int a, int b)
-    {
-        return a + b;
-    }
-}
