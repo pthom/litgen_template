634c890facbd024eadf8f29ffdaa84a26277b5ad
Pascal Thomet <pthomet@gmail.com>
2024-01-22 08:35:28 +0100

    Work on C++ example code

--- cpp_libs/DaftLib/DaftLib.h
+++ cpp_libs/DaftLib/DaftLib.h
@@ -1,14 +1,99 @@
+#include <memory>
+#include <string>
+
 namespace DaftLib
 {
+    // Simple add function (this will be the docstring)
     int add(int a, int b);
+    inline int sub(int a, int b) { return a - b; } // This is the docstring for `sub`
 
-    // In this example, the parameter v will be "Boxed" into a "BoxedInt"
-    // so that modifications can be seen from python
-    // See options.fn_params_replace_modifiable_immutable_by_boxed__regex in autogenerate_xxx.py
-    inline void inplace_multiply(int * v)
+    // A default constructor with named parameters will
+    // be automatically generated in python for structs
+    struct Point
     {
-        *v = (*v) * 2;
+        int x;
+        int y;
+
+        // The spaceship operator is supported and will generate automatically
+        // the correct comparison methods in python
+        // (__le__, __lt__, __ge__, __gt__, __eq__, __ne__)
+        auto operator<=>(const Point&) const = default;
+    };
+
+
+    // SwitchBoolValue is a C++ function that takes a bool parameter by reference and changes its value
+    // Since bool are immutable in python, we can to use a BoxedBool instead in python.
+    // See inside tools/autogenerate_bindings.py:
+    //        options.fn_params_replace_modifiable_immutable_by_boxed__regex = "^SwitchBoolValue$"
+    inline void SwitchBoolValue(bool &v) { v = !v; }
+
+    // This function will be excluded from the generated bindings
+    // See inside tools/autogenerate_bindings.py:
+    //    options.fn_exclude_by_name__regex = "^priv_"
+    inline void priv_SetOptions(bool v) {}
+
+    // The parameter priv_param will be excluded from the generated bindings
+    // since it has a default value, and is excluded via the options.
+    // See inside tools/autogenerate_bindings.py:
+    //    options.fn_params_exclude_names__regex = "^priv_"
+    inline void SetOptions(bool v, bool priv_param = false) {}
+
+    class Widget
+    {
+    public:
+        Widget() = default;
+        Widget(const Widget&) = default;
+
+        int get_value() const { return m_value; }
+        void set_value(int v) { m_value = v; }
+
+    private:
+        int m_value = 0;
+    };
+
+
+    // Python should not free the memory of the returned reference,
+    // so we will force the reference policy to be 'reference' instead of 'automatic'
+    // See
+    //        options.fn_return_force_policy_reference_for_references__regex = "Singleton$"
+    inline Widget& GetWidgetSingleton()
+    {
+        static Widget static_widget;
+        return static_widget;
     }
 
-    inline int sub(int a, int b) { return a - b; }
+
+    // This namespace will be published as a python module
+    // All functions inside this namespace will be vectorizable
+    // (see https://pthom.github.io/litgen/litgen_book/05_05_00_functions.html#vectorize-functions)
+    // See inside tools/autogenerate_bindings.py:
+    //      options.fn_namespace_vectorize__regex = "^MathFunctions$"
+    //
+    // Marche pas!!!
+    namespace MathFunctions
+    {
+        inline double log(double x) { return std::log(x); }
+        inline double deg_to_rad(double x) { return x * 3.14159265358979323846 / 180.0; }
+    }
+
+    // The virtual method of this class can be overriden in python
+    // see
+    //    options.class_override_virtual_methods_in_python__regex = "^Animal$"
+    class Animal
+    {
+    public:
+        virtual ~Animal() { }
+        virtual std::string go(int n_times) = 0;
+    };
+
+    // MaxValue will be published as max_value_int and max_value_float
+    // See inside tools/autogenerate_bindings.py:
+    //        options.fn_template_options.add_specialization(
+    //            "^MaxValue$",
+    //            ["int", "float"],
+    //            add_suffix_to_function_name=True)
+    template<typename T> T MaxValue(const std::vector<T>& values) { return *std::max_element(values.begin(), values.end());}
 }
+
+
+
--- tools/autogenerate_bindings.py
+++ tools/autogenerate_bindings.py
@@ -3,10 +3,41 @@ import os
 
 
 def my_litgen_options() -> litgen.LitgenOptions:
-    options = litgen.LitgenOptions()
     # configure your options here
-    options.namespace_root__regex = "^DaftLib$"
-    options.fn_params_replace_modifiable_immutable_by_boxed__regex = "^inplace"
+    options = litgen.LitgenOptions()
+
+    # The namespace DaftLib is the root namespace for the generated bindings
+    # (i.e. no submodule will be generated for it in the python bindings)
+    options.namespaces_root = ["^DaftLib$"]
+
+    # SwitchBoolValue is a C++ function that takes a bool parameter by reference and changes its value
+    # Since bool are immutable in python, we can to use a BoxedBool instead
+    options.fn_params_replace_modifiable_immutable_by_boxed__regex = "^SwitchBoolValue$"
+
+    # priv_ is a prefix for private functions that we don't want to expose
+    options.fn_exclude_by_name__regex = "^priv_"
+
+    # We don't want to expose the private class DaftLib::PrivateClass
+    options.fn_params_exclude_names__regex = "^priv_"
+
+    # Widget& GetWidgetSingleton()
+    # returns a reference, that python should not free.
+    # so we will force the reference policy to be 'reference' instead of 'automatic'
+    options.fn_return_force_policy_reference_for_references__regex = "Singleton$"
+
+    # The functions in the MathFunctions namespace will be also published as vectorized functions
+    options.fn_namespace_vectorize__regex = "^MathFunctions$"
+
+    # The virtual methods of this class can be overriden in python
+    options.class_override_virtual_methods_in_python__regex = "^Animal$"
+
+    #  template<typename T> T MaxValue(const std::vector<T>& values);
+    # will be published as: max_value_int and max_value_float
+    options.fn_template_options.add_specialization(
+        "^MaxValue$",
+        ["int", "float"],
+        add_suffix_to_function_name=True)
+
     return options
 
 
